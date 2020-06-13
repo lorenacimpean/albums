2130f02ba2b839d136f8a8ba319f59de4bfc2cff
lorenacimpean <lorenacimpean@rodeapps.com>
2020-06-13 20:12:13 +0300

    add models

---
+++ lib/network/album_client.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+import '../util/nothing.dart';
+import '../util/request_type.dart';
+import '../util/request_type_exception.dart';
+
+class AlbumClient {
+  //Base url
+  static const String _baseUrl = "https://jsonplaceholder.typicode.com/";
+  final Client _client;
+
+  AlbumClient(this._client);
+
+  Future<Response> request(
+      {@required RequestType requestType,
+      @required String path,
+      dynamic parameter = Nothing}) async {
+    switch (requestType) {
+      case RequestType.GET:
+        return _client.get("$_baseUrl/$path");
+      case RequestType.POST:
+        return _client.post("$_baseUrl/$path",
+            headers: {"Content-Type": "application/json"},
+            body: json.encode(parameter));
+      case RequestType.DELETE:
+        return _client.delete("$_baseUrl/$path");
+      default:
+        return throw RequestTypeNotFoundException(
+            "The HTTP request mentioned is not found");
+    }
+  }
+}
---
+++ lib/network/remote_data_source.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:albums/model/albums.dart';
+import 'package:albums/model/result.dart';
+import 'package:albums/network/album_client.dart';
+import 'package:albums/util/request_type.dart';
+import 'package:http/http.dart';
+
+class RemoteDataSource {
+  //Creating Singleton
+  RemoteDataSource._privateConstructor();
+  static final RemoteDataSource _apiResponse =
+      RemoteDataSource._privateConstructor();
+  factory RemoteDataSource() => _apiResponse;
+  AlbumClient client = AlbumClient(Client());
+
+  Future<Result> getAlbums() async {
+    try {
+      final response =
+          await client.request(requestType: RequestType.GET, path: "albums");
+      if (response.statusCode == 200) {
+        return Result<AlbumList>.success(AlbumList.fromRawJson(response.body));
+      } else {
+        return Result.error("Album list is not available");
+      }
+    } catch (error) {
+      return Result.error("Something went wrong!");
+    }
+  }
+}
