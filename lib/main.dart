c7094ca3837c6a2b63e48243b2606e043a9b2281
lorenacimpean <lorenacimpean@rodeapps.com>
2020-08-14 12:43:47 +0300

    finished setting up onesignal and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:albums/themes/app_theme.dart';
 import 'package:albums/ui/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // init OneSignal
+  OneSignal.shared.init("da85f1be-ea8a-4e55-8688-5b468e25d376", iOSSettings: {
+    OSiOSSettings.autoPrompt: false,
+    OSiOSSettings.inAppLaunchUrl: true
+  });
+  OneSignal.shared
+      .setInFocusDisplayType(OSNotificationDisplayType.notification);
+
   runApp(AlbumsApp());
 }
 
